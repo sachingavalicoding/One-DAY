364f6a1f319c34e9fd59c727faf9e83f2dea65b9
Sachin Gavali <sachingavali321@gmail.com>
2023-06-02 08:13:18 +0530

    Practice JavaScript

---
+++ DAY-3/sum.js
@@ -0,0 +1,12 @@
+
+/* let sum = (a,b) => {
+    return a + b;
+}
+console.log(sum(10,20)); */
+
+let sum = (a,b) =>  a + b;
+console.log(sum(10,20));
+console.log(sum(30,20));
+console.log(sum(60,80));
+console.log(sum(90,290));
+console.log(sum(106,340));
