c9662bcf4129533c721b424393a3181c543ad3a1
Sachin Gavali <sachingavali321@gmail.com>
2023-06-28 15:07:20 +0530

    Js Practice Questions

---
+++ PracticeQues/alternateIndex.js
@@ -0,0 +1,4 @@
+let arr = [1,2,3,4,5,6,7,8,9,10];
+for(let i = 1; i < arr.length; i = i + 2){
+    console.log(arr[i]);
+}
---
+++ PracticeQues/names.js
@@ -0,0 +1,51 @@
+let Name = ['sa', 'ba', 'hj', 'ad', 'hg'];
+/* for(let i = 0; i < Name.length; i++){
+ console.log(Name[i]);
+} */
+/* 
+let i = 0;
+ while(i < Name.length){
+    console.log(Name[i]);
+    i = i + 1;
+} */
+/* console.log(Name); */
+/* Name.forEach((i) => {
+    console.log(i);
+}) */
+
+/* Name.push(89); */
+/* Name.unshift('hey'); */
+/* Name.pop(); */
+/* Name.shift(); */
+/* Name.map((i)=>{
+    console.log(i)
+}) */
+let str = 'SAchin';
+console.log(Name);
+
+/* console.log(str.toLowerCase());
+console.log(str.toUpperCase()); */
+/* console.log(str.replace('in','NA')) */
+
+/* console.log(str.split('c',2)) */
+
+/* console.log(str.match('S')) */
+/* console.log(str.charAt()); */
+/* console.log(str.concat('GAvali ')); */
+/* console.log(str.indexOf('S')); */
+
+
+/* let s = 'Pari__Hu__Main';
+console.log(s.replace('Pari__Hu__Main','Pari__Nahi__Hu__Main')) */
+/* let s = 'Pari__Hu__Main';
+let count = 0;
+for(let i = 0; i < s.length; i++){
+    if(s.charAt(i) != '_'){
+     count++;
+    }
+} */
+/* console.log( 'Total Char  = ', count);    */
+/* console.log(`Total Count = ${count}`);    */
+let s = 'Pari__Hu__Main';
+/* console.log(s.split('__',2)) */
+console.log(s.slice(0,5))
